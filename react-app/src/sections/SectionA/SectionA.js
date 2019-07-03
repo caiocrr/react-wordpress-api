3064288ce3969deff4d5ffa4d6f8ff7c8c73ac92
Caio Ramos <caiocrr@poli.ufrj.br>
2019-07-03 01:53:30 -0300

    chamada api com serviço http

--- react-app/src/sections/SectionA/SectionA.js
+++ react-app/src/sections/SectionA/SectionA.js
@@ -3,8 +3,15 @@ import { Container, Col, Row } from "react-bootstrap";
 import "./style.css";
 import TitleSection from "../../components/TitleSection";
 import DescSection from "../../components/DescSection";
+import { http } from "../../services/http";
 class SectionA extends Component {
   state = {};
+  componentDidMount() {
+    console.log("AEEEE");
+    http.get("section_a").then(x => {
+      console.log(x);
+    });
+  }
   render() {
     return (
       <div className="dan-section pt-5 pb-5" id="sectionA">
---
+++ react-app/src/services/http.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+const API_URL = "wp-json/acf/v3";
+
+class Http {
+  api = axios.create({
+    baseURL: `${API_URL}`
+  });
+
+  constructor() {
+    this.api.interceptors.request.use(
+      async config => {
+        return config;
+      },
+      error => {
+        // I cand handle a request with errors here
+        return Promise.reject(error);
+      }
+    );
+  }
+  get = route => {
+    return this.api.get(route);
+  };
+  post = (route, body) => {
+    return this.api.post(route, body);
+  };
+  put = (route, body) => {
+    return this.api.put(route, body);
+  };
+}
+
+export const http = new Http();
